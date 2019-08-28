1de7a623f0e0613078cec1079f20d0fe11d97767
Kate Laney <kate.a.laney@gmail.com>
2019-08-28 13:41:19 -0400

    established basic HTML and pseudocoded JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,56 @@
+// Define objects for character variables.
+
+var aragorn = {
+    hitpoints = 100,
+    attack = 450,
+    counter = 450,
+}
+
+var eowyn = {
+    hitpoints = 100,
+    attack = 200,
+    counter = 150,
+}
+
+var gimli = {
+    hitpoints = 100,
+    attack = 300,
+    counter = 350
+}
+
+var galadriel = {
+    hitpoints = 100,
+    attack = 600,
+    counter = 150,
+}
+
+// Define an on.click function to select the character ID.
+
+
+// Define an on.click function to select the enemy ID.
+
+// Define an on.click function to start the attack function.
+
+/* Attack function:
+
+    If character attack
+    return enemy HP loss
+    if enemy HP < 0
+    return enemy dies
+    character ID disappears
+
+    If character attack
+    return character attack increases (new attack = attack + new attack)
+    
+    If enemy attack
+    return character counter attack
+
+    If enemy attack
+    return character HP loss
+    if character HP < 0
+    return character dies
+    game resets on character death
+
+*/
+
+// 
