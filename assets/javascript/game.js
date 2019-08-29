cf98e5c898328a6b7f37ac0d760912e16e64cfab
Kate Laney <kate.a.laney@gmail.com>
2019-08-29 16:08:37 -0400

    HTML updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,41 +9,47 @@ var death = [];
 
 // Define objects for character variables.
 
-var aragorn = {
-    hitpoints = 100,
-    attack = 450,
-    counter = 450,
-}
+var characters = [{
+    name = aragorn,
+    ahitpoints = 100,
+    aattack = 450,
+    acounter = 450,
 
-var eowyn = {
-    hitpoints = 100,
-    attack = 200,
-    counter = 150,
-}
 
-var gimli = {
-    hitpoints = 100,
-    attack = 300,
-    counter = 350
-}
+    name = eowyn,
+    ehitpoints = 100,
+    eattack = 200,
+    ecounter = 150,
 
-var galadriel = {
-    hitpoints = 100,
-    attack = 600,
-    counter = 150,
-}
+
+    name = gimli,
+    ghitpoints = 100,
+    gattack = 300,
+    gcounter = 350,
+
+
+    name = galadriel,
+    gahitpoints = 100,
+    gaattack = 600,
+    gacounter = 150,
+}]
 
 // Define an on.click function to select the character ID and move to arena.
-
+$("#galadriel").click(function() {
+    ("#galadriel").hide();
+  });
 
 
 // Define an on.click function to select the enemy ID and move to arena.
 
 // Define an on.click function to select the "Attack" button and start the attack function.
 
-/* Attack function:
+  var attack = function() {
+      
+  }
+    
 
-    If character attack
+/*  If character attack
     return enemy HP loss
     if enemy HP < 0
     return enemy dies
