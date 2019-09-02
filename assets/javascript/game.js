de6c57755eeb7379d8a9f9c83b0174d0bd342b44
Kate Laney <kate.a.laney@gmail.com>
2019-09-02 14:56:53 -0400

    it's all broken again

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,7 @@ var score = 0;
 // Generate a random number between 19 and 120.
 
 var scoreToBeat = Math.floor(Math.random() * 101) + 19;
+$("#randomNumber").text("Random Number: " + scoreToBeat);
 
 // Testing random number generator.
 console.log(scoreToBeat);
@@ -23,50 +24,51 @@ var crystal4 = Math.floor(Math.random() * 12) + 1;
 // Testing random number generator.
 console.log(crystal1)
 
-// Create an onclick function to set the random number.
-$("#randomNumber").on("click", function () {
-    $("#randomNumber").text("Click for Random Number: " + scoreToBeat);
-});
-
 // Create an onclick function for when a player selects a crystal to add to score and update the score.
 
-$("#roseGold").on("click", function() {
+$("#roseGold").click(function() {
     score = score + crystal1;
     $("#yourScore").text("Your Score: " + score);
     if (score === scoreToBeat) {
         win();
-    } else if (score > scoreToBeat);
+    } else if (score > scoreToBeat); {
         loss();
-    }  
+    }
+    }
 );
 
-$("#purple").on("click", function() {
+
+$("#purple").click(function() {
     score = score + crystal2;
     $("#yourScore").text("Your Score: " + score);
     if (score === scoreToBeat) {
         win();
-    } else if (score > scoreToBeat);
+    } else if (score > scoreToBeat); {
         loss();
+    }
     }
 );
 
-$("#clear").on("click", function() {
+
+$("#clear").click(function() {
     score = score + crystal3;
     $("#yourScore").text("Your Score: " + score);
     if (score === scoreToBeat) {
         win();
-    } else if (score > scoreToBeat);
+    } else if (score > scoreToBeat); {
         loss();
+    }
     }
 );
 
-$("#rainbow").on("click", function() {
+$("#rainbow").click(function() {
     score = score + crystal4;
     $("#yourScore").text("Your Score: " + score);
     if (score === scoreToBeat) {
         win();
-    } else if (score > scoreToBeat);
+    } else if (score > scoreToBeat); {
         loss();
+    }
     }
 );
 
@@ -76,7 +78,7 @@ function reset () {
     score = 0;
     $("#yourScore").text("Your Score: " + score);
     scoreToBeat = Math.floor(Math.random() * 101) + 19;
-    $("#randomNumber").text("Click for Random Number: " + scoreToBeat);
+    $("#randomNumber").text("Random Number: " + scoreToBeat);
 }
 
 // Update the win counter.
@@ -90,7 +92,7 @@ function win() {
 // Update the loss counter.
 
 function loss() {
-    loss++;
+    losses++;
     $("#losses").text("Losses: " + losses);
     reset();
 }
